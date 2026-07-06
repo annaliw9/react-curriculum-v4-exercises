e2b2c80da39ec6cb08340ff405e25628a1204dc8
Shuna Li <shuna.li@hotmail.com>
2026-07-06 16:02:39 -0700

    adding lesson-01 exercise

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,29 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  const name = 'Shuna Li';
+  const age = 29;
+  const hobbies = ['Zumba', 'Movies', 'Traveling', 'Photography'];
+
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h3>About Me</h3>
+      <p>
+        My name is {name} and I&apos;m {age}. I&apos;m a full-stack software
+        engineer from San Francisco building seamless web applications, from
+        interactive front-ends to scalable back-end systems.
+      </p>
+      <p>
+        I&apos;m passionate about turning ideas into full-stack performance. I
+        blend front-end design and back-end logic to build responsive,
+        accessible, and scalable web applications that truly delight users.
+      </p>
+      <p>When I&apos;m not coding, I enjoy:</p>
+      <ul>
+        {hobbies.map((e) => (
+          <li key={e}>{e}</li>
+        ))}
+      </ul>
     </div>
   );
 }
