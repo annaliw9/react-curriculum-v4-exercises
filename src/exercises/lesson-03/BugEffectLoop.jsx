d2dfffb8bfc57d0295a94a4cd96aa344e05450da
Shuna Li <shuna.li@hotmail.com>
2026-07-21 10:07:51 -0700

    adding exercises for lesson-03

--- src/exercises/lesson-03/BugEffectLoop.jsx
+++ src/exercises/lesson-03/BugEffectLoop.jsx
@@ -14,11 +14,13 @@ export default function BugEffectLoop() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setCount(count + 1);
-  });
+    //  setCount(count + 1);
+    setCount((prev) => prev + 1);
+  }, []);
 
   return <p>Bug 1 Count: {count}</p>;
 }
 
-// Explanation:
-// (Write your explanation here)
+//Explanation:
+//The problem with this component is that the useEffect hook is running on every render, which causes the component to enter an infinite loop of re-renders. To fix this, we need to add an empty dependency array as the second argument to useEffect. This tells React to only run the effect once when the component mounts, instead of on every render. Also, we should use the functional form of setCount, which takes a function that receives the previous state and returns the new state. This ensures that we are always working witht the most up-to-date value of count, even if multiple updates are queued.
+//Count is 2 in the UI because of the strict mode in React 18. In strict mode, React intentionally double-invokes certain lifecycle methods (including useEffect) to help identify side effects and ensure that components are resilient to being mounted and unmounted multiple times. This means that the effect runs twice during development, leading to the count being incremented twice and resulting in a final value of 2.
--- src/exercises/lesson-03/BugProps.jsx
+++ src/exercises/lesson-03/BugProps.jsx
@@ -12,11 +12,14 @@
   Use the commented "Explanation" section at the bottom of this lesson's components.
 */
 
+import { useState } from 'react';
 export default function BugProps({ name = 'friend' }) {
-  let message = 'Hello, ' + name;
+  // let message = 'Hello, ' + name;
+  const [message, setMessage] = useState('Hello,' + name);
 
   function handleChange() {
-    message = 'Hi, ' + name + '!';
+    // message = 'Hi, ' + name + '!';
+    setMessage('Hi, ' + name + '!');
   }
 
   return (
@@ -28,4 +31,4 @@ export default function BugProps({ name = 'friend' }) {
 }
 
 // Explanation:
-// (Write your explanation here)
+// The probleme with this component is that the message variable is being declared as a regular/local variable, which means that React does not track its value. When the handleChange function is called, it updates the message variable, but React does not know that it needs to re-render the component to reflect the new value. To fix this, we need to use the useState hook to decleare the message variable as a state variable. This way, when we call setMessage to update the value, React will know to re-render the component and display the new message.
